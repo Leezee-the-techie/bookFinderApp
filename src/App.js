6fdfbdd0f62ee85f993b0b70399cbbbae930e26a
Leezee-the-techie <nasuaelizabeth@gmail.com>
2024-10-10 03:13:03 -0700

    added bookfinder component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Bookfinder from './Bookfinder';
 
 function App() {
   return (
     <>
-    
+    <Bookfinder/>
     </>
   );
 }
---
+++ src/Bookfinder.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Bookfinder(){
+
+
+    return(
+        <>
+        <h1>Welcome to BookFinder</h1>
+        </>
+    );
+};
